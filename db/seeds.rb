58df22e4d66fe82c3bb4755e3a111816f29908a8
Charli Bregnballe <charli@kompjuter.dk>
2016-12-04 20:34:45 +0100

    Implemented edit functionality for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,12 @@
                           first_name: "Jon",
                           last_name: "Snow")
 
+AdminUser.create(email: "admin@test.com",
+                          password: "qwerty",
+                          password_confirmation: "qwerty",
+                          first_name: "Admin",
+                          last_name: "Name")
+
 puts "1 user created"
 
 100.times do |post|
