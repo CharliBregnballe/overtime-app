d2f7222ce7f279e7c5f9fca0daae5a890a434321
Charli Bregnballe <charli@kompjuter.dk>
2016-12-08 22:32:51 +0100

    Added hours requested to post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ AdminUser.create(email: "admin@test.com",
                           first_name: "Admin",
                           last_name: "Name")
 
-puts "1 user created"
+puts "2 user created"
 
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 
