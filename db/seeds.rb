6809d68bcaf9435d7a1765ffbf307d280e17905f
Charli Bregnballe <charli@kompjuter.dk>
2017-03-06 20:53:16 +0100

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,14 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin User created"
 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+
+puts "3 audit logs created"
+
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer. Sætningen blev gjordt kendt i 1960'erne med lanceringen af Letraset-ark, som indeholdt afsnit med Lorem Ipsum, og senere med layoutprogrammer som Aldus PageMaker, som også indeholdt en udgave af Lorem Ipsum.", user_id: @user.id, overtime_request: 2.5)
 end
@@ -23,9 +31,3 @@ end
 
 puts "100 posts have been created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-
-puts "3 audit logs created"
